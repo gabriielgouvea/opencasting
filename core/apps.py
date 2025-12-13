20c2d93b8aa0a1b3af58ca8e9d6d2510221aa169
Gabriel Gouvea <gabriielgouvea@gmail.com>
2025-12-13 15:42:13 -0300

    Fix: Adiciona path explicito no apps.py para corrigir erro do servidor

--- core/apps.py
+++ core/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
+import os
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+    
+    # Essa linha é a MÁGICA que conserta o erro no PythonAnywhere
+    # Ela diz: "O caminho real deste app é EXATAMENTE onde este arquivo está"
+    path = os.path.dirname(os.path.abspath(__file__))
